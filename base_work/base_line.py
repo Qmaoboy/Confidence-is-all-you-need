928a391c95c8097851e6c6779a43d1665105b980
Qmaoboy <hpjhuang@gmail.com>
2024-06-27 14:42:23 +0800

    update

---
+++ base_work/base_line.py
@@ -0,0 +1,133 @@
+
+from LLM_API import *
+from qadataset_dataloader import qadataset_dataloader
+from rouge_score import rouge_scorer
+from prompt_strategy import prompter
+from tqdm import tqdm
+from util import *
+import multiprocessing as mp
+from sklearn.metrics import roc_auc_score
+
+if os.path.isfile("api_key.yml"):
+    with open("api_key.yml","r") as f:
+        key=yaml.safe_load(f)
+
+
+def Load_cot_exmple():
+    with open() as f:
+        data=f.read().strip()
+    print(data)
+
+def question_to_prompt(question,task="self_polish",stretagy='self_polish'):
+    p=prompter()
+    p.setup_task(task)
+    return p.get_prompt(question,[],stretagy)
+
+
+def Get_auroc(accuracy,confidence_scores):
+    y_true=np.where(np.array(accuracy) < 0.3,0,1)
+    return roc_auc_score(np.array(y_true), np.array(confidence_scores))
+
+def get_from_gpt(prompt,parser_stretagy):
+    result=GPT_API('gpt-3.5-turbo-0125',key['hugginface']['token'],parser_stretagy,prompt).generate()
+    return result
+
+def ans_scorer(new_ans,original_ans):
+    ## Compare Result
+    result=rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True).score(new_ans,original_ans)['rougeL'].fmeasure
+    return result
+
+def rewrite_worker(share_list,idx,original_question,ground_truth,documnet,baseline):
+
+    if baseline =="vanilla":
+        Answer_result=get_from_gpt(question_to_prompt([original_question],'pure','vanilla'),'confidence')
+        if Answer_result is not None:
+            rouge_score=ans_scorer(Answer_result['Answer'],ground_truth)
+            share_list.append({
+                'Id':idx,
+                'Original_question':original_question,
+                'Documnet':documnet,
+                'Ground_truth':ground_truth,
+                'Answer':Answer_result['Answer'],
+                'Confidence':Answer_result['Confidence'],
+                'rouge_score':rouge_score,
+            })
+
+    elif baseline =="self_polish":
+        ###### self polish iterate refine question
+        old_refine_question=original_question
+        for _ in (p:=tqdm(range(3),leave=False)):
+            new_question=get_from_gpt(question_to_prompt([old_refine_question],'self_polish','self_polish'),'self_polish')
+            if old_refine_question==new_question['New_Question']: ## converge
+                break
+            else:
+                old_refine_question=new_question['New_Question']
+
+        Answer_result=get_from_gpt(question_to_prompt([new_question["New_Question"]],'QA','vanilla'),'confidence')
+        if Answer_result is not None:
+            rouge_score=ans_scorer(Answer_result['Answer'],ground_truth)
+            share_list.append({
+                'Id':idx,
+                'Original_question':original_question,
+                'New_Question':new_question['New_Question'],
+                'Documnet':documnet,
+                'Ground_truth':ground_truth,
+                'Answer':Answer_result['Answer'],
+                'Confidence':Answer_result['Confidence'],
+                'rouge_score':rouge_score,
+            })
+
+    elif baseline =="RaR":
+        Answer_result=get_from_gpt(question_to_prompt([original_question],'RaR','RaR'),'RaR')
+        # Answer_result=get_from_gpt(question_to_prompt([new_question],'RaR','RaR'),'confidence')
+        if Answer_result is not None:
+            rouge_score=ans_scorer(Answer_result['Answer'],ground_truth)
+
+            share_list.append({
+                'Id':idx,
+                'Original_question':original_question,
+                'Expanded_question':Answer_result['Expanded_Question'],
+                'Documnet':documnet,
+                'Ground_truth':ground_truth,
+                'Answer':Answer_result['Answer'],
+                'Confidence':Answer_result['Confidence'],
+                'rouge_score':rouge_score,
+            })
+
+def evaluate_result(baseline):
+    with open(f'{baseline}.json','r') as f:
+        data=json.load(f)
+
+    acc=np.array([i['rouge_score'] for i in data])
+    conf=np.array([i['Confidence'] for i in data])
+    ece_score=np.abs(acc-conf)
+    print(f"rouge_score mean :{np.mean(acc)}")
+    print(f"ECE mean :{np.mean(ece_score)}")
+    print(f"Auroc mean :{Get_auroc(acc,conf)}")
+
+def main(baseline):
+
+    train_dataloader=qadataset_dataloader(dataset_path="din0s/asqa",split='dev',batch_size=1).trainloader
+    share_list=mp.Manager().list()
+    mp_pool=mp.Pool(processes=mp.cpu_count())
+    for idx,(batch,Ground_truth) in enumerate(progress:=tqdm(train_dataloader)):
+        original_question=[i[0] for i in batch]
+        document=[search_wikipedia_byurl(i[1]) if i[2] else i[1] for i in batch]
+        args=[(share_list,idx,q,gt,doc,baseline) for idx,(q,gt,doc) in enumerate(zip(original_question,Ground_truth,document))]
+        mp_pool.starmap(rewrite_worker,args)
+        if share_list:
+            progress.set_description_str(f"Processing {len(share_list)} batch rouge {np.mean(np.array([i['rouge_score'] for i in share_list]))}")
+        if len(share_list)>=50:
+            break
+
+    mp_pool.close()
+    mp_pool.join()
+
+    with open(f'{baseline}.json','w+') as f:
+        json.dump(list(share_list),f)
+
+
+if __name__=="__main__":
+    baseline='self_polish'
+    main(baseline)
+    evaluate_result(baseline)
---
+++ base_work/qadataset_dataloader.py
@@ -0,0 +1,167 @@
+from datasets import load_dataset
+from torch.utils.data import DataLoader,Dataset
+from datetime import datetime
+from util import *
+import os,re
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+logger = setup_logger(f'log/response_{datetime.now().strftime("%Y%m%d")}.log')
+
+class qadataset_dataloader:
+    def __init__(self,dataset_path="din0s/asqa",split='train',batch_size=2,shuffle=True):
+        super(qadataset_dataloader,self).__init__()
+        self.dataset_path=dataset_path
+        self.split=split
+        self.batch_size=batch_size
+        self.shuffle=shuffle
+        self.get_loader()
+
+
+    def setup_collect_fn(self): ## "gsm8k",DateUnd,Prf-Law,Biz-Ethics
+        if self.dataset_path=="din0s/asqa":
+            dataset_name="din0s/asqa"
+            self.collect_fn=self.asqa_collate_fn
+            self.dataset = load_dataset(dataset_name)
+
+        elif self.dataset_path=="natural_questions": # Short Ans ; EM Score
+            self.collect_fn=self.naturalqa_collect_fn
+            self.dataset=self.Load_natural_qa(20)
+
+        elif self.dataset_path=="ChilleD/StrategyQA": # True/ False
+            dataset_name="ChilleD/StrategyQA"
+            self.collect_fn=self.stretagy_qa_collect_fn
+            self.dataset = load_dataset(self.dataset_path)
+
+        elif self.dataset_path=="gsm8k":
+            dataset_name="gsm8k"
+            self.collect_fn=self.gsm8k_collect_fn
+            self.dataset = load_dataset(self.dataset_path,"main")
+
+        elif self.dataset_path=="triviaQA":
+            dataset_name="mandarjoshi/trivia_qa"
+            self.collect_fn=self.triviaqa_collect_fn
+            self.dataset = load_dataset(dataset_name,"rc.wikipedia")
+
+    def get_loader(self):
+
+        self.setup_collect_fn()
+
+        if self.split in self.dataset:
+            self.trainloader=DataLoader(self.dataset[self.split],batch_size=self.batch_size,shuffle=self.shuffle,collate_fn=self.collect_fn,num_workers=1,drop_last=True)
+        else:
+            self.trainloader = DataLoader(self.dataset, batch_size=self.batch_size, collate_fn=self.collect_fn,shuffle=self.shuffle)
+
+    def Load_natural_qa(self,idx):
+        qa_dict=[]
+        for i in range(idx):
+            datapath=f"natural_qa_dataet/natual_qa_{i}.json"
+            old_data=load_checkpoint(datapath)
+            qa_dict+=old_data
+            logger.info(f"Load Data from {datapath}")
+        return qa_dict
+
+    def asqa_collate_fn(self,batch): ## long_ans
+
+        isurl=True
+        res=[[i['ambiguous_question'],[i['wikipages'][0]['url']],isurl] for i in batch]
+        long_ans=[i['annotations'][0]['long_answer'] for i in batch]
+        return res,long_ans
+
+    def naturalqa_collect_fn(self,batch): ## Long ans or short ans
+        res=[[i['Question'],i['Document'],i['isurl']] for i in batch]
+        short_ans=["".join(i['Short_answer']) for i in batch]
+        long_ans=["".join(i['long_answer'])for i in batch]
+        return res,short_ans
+
+    def stretagy_qa_collect_fn(self,batch): ## True / False
+        res=[]
+        isurl=False
+        res=[[i['question'],i['facts'],isurl]for i in batch]
+        answer=[i['answer'] for i in batch]
+        return res,answer
+
+    def gsm8k_collect_fn(self,batch):
+
+        res=[[i["question"],"",False] for i in batch]
+        ans=[i['answer']for i in batch]
+        ground_Truth=[re.findall(r'\d+',i['answer']).pop()for i in batch]
+
+        return res,ans
+
+    def triviaqa_collect_fn(self,batch):
+        res=[]
+        print(batch)
+        return batch
+
+class eval_dataloader:
+    def __init__(self,dataset_path,batch_size,purpose='compare',tokenizer="",shuffle=False) -> None:
+        self.tokenizer = tokenizer
+        self.shuffle=shuffle
+        self.dataset = load_checkpoint(dataset_path)
+        if purpose=="compare":
+            self.loader=DataLoader(list(self.dataset),batch_size=batch_size,collate_fn=self.parallel_simi_collate_fn,shuffle=self.shuffle,num_workers=1,drop_last=True,persistent_workers=True)
+        elif purpose=="acc":
+            self.loader=DataLoader(list(self.dataset),batch_size=batch_size,collate_fn=self.acc_collate_fn,shuffle=self.shuffle,num_workers=1,drop_last=True,persistent_workers=True)
+
+        elif purpose=="refine":
+            sep_index=int(len(self.dataset)*0.8)
+            # sep_index=10
+            self.trainloader=DataLoader(list(self.dataset)[:sep_index],batch_size=batch_size,collate_fn=self.refine_collect_fn,shuffle=self.shuffle,num_workers=1,drop_last=True,persistent_workers=True)
+            self.testloader=DataLoader(list(self.dataset)[sep_index:],batch_size=batch_size,collate_fn=self.refine_collect_fn,shuffle=self.shuffle,num_workers=1,drop_last=True,persistent_workers=True)
+
+    def acc_collate_fn(self,batch):
+        # print(batch)
+        ans=[i['Answer'] for i in batch]
+        ground_Truth=[i['Ground_Truth']for i in batch]
+        res=[[i['Document'],i['Doc_Ans_simi'],i['Confidence'],i['Prompt']]for i in batch]
+        return res,ans,ground_Truth
+
+    def parallel_simi_collate_fn(self,batch):
+        ans=[i['Answer'] for i in batch]
+        long_ans=[i['Ground_Truth']for i in batch]
+        res=[[i['Question'],i['Document'],i['Confidence'],i['Explanation'],i['Prompt']] for i in batch]
+        return res,ans,long_ans
+
+    def refine_collect_fn(self,batch):
+
+        ans=[i['Answer'] for i in batch]
+        ground_Truth=[i['Ground_Truth']for i in batch]
+
+        Document=[i['Document']for i in batch]
+        Confidence=[i['Confidence']for i in batch]
+        prompt=[i['Prompt'] for i in batch]
+
+        Instruct_Example="{Instruction:[Your Instruction Here]}"
+        basic_instruct="generate Instruction for the given and Question and Instruction"
+
+        instruc_gpt_='Below is an instruction that describes a task. Write a better Instruction base on the Instruction and Question.'
+
+        # instruct=[f'''<s>[INST] <<SYS>>You are a Instruction generator excel at rewriting the basic_instruction according to the question in order to generate more detail Answer for long form QA task. Only Give me the new_Instruction, do not give any other infomation.<</SYS>>\nbasic_instruction:"{i['Prompt']['Instruction']}"\nQuestion:{i['Prompt']['Question']}\n
+        # [/INST]new_Instruction:\n''' for i in batch]
+
+        instruct=[f'''[INST] <<SYS>>
+                    Rewrite the following basic instruction to help a large language model generate a more detailed and comprehensive answer for a long-form QA task. Ensure the rewritten instruction is clear and concise, prompting the model to provide a thorough and well-structured response of at least 300 tokens to the given question. The new instruction should be within 256 tokens.
+
+                    Basic Instruction: "{i['Prompt']['Instruction']}"
+                    Question: "{i['Prompt']['Question']}"
+                    [/INST]
+                    new instruction:''' for i in batch]
+
+
+        # gpt2_instruct=[f"{basic_instruct}\n\n \#\#\#Instruction:\n {i['Prompt']['Instruction']},Question:{i['Prompt']['Question']}\,Now Start to generate better Instruction :\n" for i in batch]
+
+        ground_Truth_token=self.tokenizer(ground_Truth,padding=False,truncation=True,max_length=512)
+        instruct_token=self.tokenizer(instruct,padding=False,truncation=True,max_length=512)
+
+        return prompt,instruct,instruct_token,ans,ground_Truth,ground_Truth_token,Confidence,Document
+
+
+if __name__=="__main__":
+    qa_loader=qadataset_dataloader("din0s/asqa",split='train',batch_size=1).loader
+    for batch,short_ans in qa_loader:
+
+        break
+
+    # simi_loader=eval_dataloader("response_result/ChilleD_StrategyQA_gpt-3.5-turbo-0125_vanilla_QA_2024_05_12.json",1,'compare').loader
+    # for res,ans,long_ans in simi_loader:
+    #     print(ans)
+
