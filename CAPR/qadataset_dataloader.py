5f408134628fbfa08201dc821ed4f51e90d4659f
Qmaoboy <hpjhuang@gmail.com>
2024-07-15 22:27:28 +0800

    update

--- CAPR/qadataset_dataloader.py
+++ CAPR/qadataset_dataloader.py
@@ -1,20 +1,17 @@
 from datasets import load_dataset
-from torch.utils.data import DataLoader,Dataset
+from torch.utils.data import DataLoader
 from datetime import datetime
 from util import *
 import os,re
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-logger = setup_logger(f'log/response_{datetime.now().strftime("%Y%m%d")}.log')
 
 class qadataset_dataloader:
-    def __init__(self,dataset_path="din0s/asqa",split='train',batch_size=2,shuffle=True):
+    def __init__(self,dataset_path="din0s/asqa",split='train',batch_size=1,shuffle=True):
         super(qadataset_dataloader,self).__init__()
         self.dataset_path=dataset_path
         self.split=split
         self.batch_size=batch_size
         self.shuffle=shuffle
         self.get_loader()
-
 
     def setup_collect_fn(self): ## "gsm8k",DateUnd,Prf-Law,Biz-Ethics
         if self.dataset_path=="din0s/asqa":
@@ -37,18 +34,17 @@ class qadataset_dataloader:
             self.dataset = load_dataset(self.dataset_path,"main")
 
         elif self.dataset_path=="triviaQA":
-            dataset_name="mandarjoshi/trivia_qa"
+            dataset_name="lucadiliello/triviaqa"
             self.collect_fn=self.triviaqa_collect_fn
-            self.dataset = load_dataset(dataset_name,"rc.wikipedia")
+            self.dataset = load_dataset(dataset_name)
 
     def get_loader(self):
 
         self.setup_collect_fn()
-
         if self.split in self.dataset:
             self.trainloader=DataLoader(self.dataset[self.split],batch_size=self.batch_size,shuffle=self.shuffle,collate_fn=self.collect_fn,num_workers=1,drop_last=True)
         else:
-            self.trainloader = DataLoader(self.dataset, batch_size=self.batch_size, collate_fn=self.collect_fn,shuffle=self.shuffle)
+            self.trainloader = DataLoader(self.dataset, batch_size=self.batch_size, collate_fn=self.collect_fn,shuffle=self.shuffle,num_workers=1,drop_last=True)
 
     def Load_natural_qa(self,idx):
         qa_dict=[]
@@ -56,13 +52,12 @@ class qadataset_dataloader:
             datapath=f"natural_qa_dataet/natual_qa_{i}.json"
             old_data=load_checkpoint(datapath)
             qa_dict+=old_data
-            logger.info(f"Load Data from {datapath}")
         return qa_dict
 
     def asqa_collate_fn(self,batch): ## long_ans
 
         isurl=True
-        res=[[i['ambiguous_question'],[i['wikipages'][0]['url']],isurl] for i in batch]
+        res=[[i['ambiguous_question'],i['wikipages'][0]['url'],isurl] for i in batch]
         long_ans=[i['annotations'][0]['long_answer'] for i in batch]
         return res,long_ans
 
@@ -88,9 +83,10 @@ class qadataset_dataloader:
         return res,ans
 
     def triviaqa_collect_fn(self,batch):
-        res=[]
-        print(batch)
-        return batch
+        res=[[i['question'],i['context'].replace("[PAR]","").replace("[DOC]","").replace("[TLE]",""),False] for i in batch]
+        ans=[sorted(i['answers'],key=lambda x:len(x),reverse=False)[-1] for i in batch]
+
+        return res,ans
 
 class eval_dataloader:
     def __init__(self,dataset_path,batch_size,purpose='compare',tokenizer="",shuffle=False) -> None:
@@ -98,15 +94,9 @@ class eval_dataloader:
         self.shuffle=shuffle
         self.dataset = load_checkpoint(dataset_path)
         if purpose=="compare":
-            self.loader=DataLoader(list(self.dataset),batch_size=batch_size,collate_fn=self.parallel_simi_collate_fn,shuffle=self.shuffle,num_workers=1,drop_last=True,persistent_workers=True)
+            self.loader=DataLoader(list(self.dataset),batch_size=len(self.dataset),collate_fn=self.parallel_simi_collate_fn,shuffle=self.shuffle,drop_last=True)
         elif purpose=="acc":
-            self.loader=DataLoader(list(self.dataset),batch_size=batch_size,collate_fn=self.acc_collate_fn,shuffle=self.shuffle,num_workers=1,drop_last=True,persistent_workers=True)
-
-        elif purpose=="refine":
-            sep_index=int(len(self.dataset)*0.8)
-            # sep_index=10
-            self.trainloader=DataLoader(list(self.dataset)[:sep_index],batch_size=batch_size,collate_fn=self.refine_collect_fn,shuffle=self.shuffle,num_workers=1,drop_last=True,persistent_workers=True)
-            self.testloader=DataLoader(list(self.dataset)[sep_index:],batch_size=batch_size,collate_fn=self.refine_collect_fn,shuffle=self.shuffle,num_workers=1,drop_last=True,persistent_workers=True)
+            self.loader=DataLoader(list(self.dataset),batch_size=len(self.dataset),collate_fn=self.acc_collate_fn,shuffle=self.shuffle,drop_last=True)
 
     def acc_collate_fn(self,batch):
         # print(batch)
@@ -121,33 +111,12 @@ class eval_dataloader:
         res=[[i['Question'],i['Document'],i['Confidence'],i['Explanation'],i['Prompt']] for i in batch]
         return res,ans,long_ans
 
-    def refine_collect_fn(self,batch):
-
-        ans=[i['Answer'] for i in batch]
-        ground_Truth=[i['Ground_Truth']for i in batch]
-
-        Document=[i['Document']for i in batch]
-        Confidence=[i['Confidence']for i in batch]
-        prompt=[i['Prompt'] for i in batch]
-
-        instruct=[f'''[INST] <<SYS>>
-                    Rewrite the following basic instruction to help a large language model generate a more detailed and comprehensive answer for a long-form QA task. Ensure the rewritten instruction is clear and concise, prompting the model to provide a thorough and well-structured response of at least 300 tokens to the given question. The new instruction should be within 256 tokens.
-
-                    Basic Instruction: "{i['Prompt']['Instruction']}"
-                    Question: "{i['Prompt']['Question']}"
-                    [/INST]
-                    new instruction:''' for i in batch]
-
-        ground_Truth_token=self.tokenizer(ground_Truth,padding=False,truncation=True,max_length=512)
-        instruct_token=self.tokenizer(instruct,padding=False,truncation=True,max_length=512)
-
-        return prompt,instruct,instruct_token,ans,ground_Truth,ground_Truth_token,Confidence,Document
-
 
 if __name__=="__main__":
-    qa_loader=qadataset_dataloader("din0s/asqa",split='dev',batch_size=1).loader
-    for batch,short_ans in qa_loader:
-
+    qa_loader=qadataset_dataloader("triviaQA",split='train',batch_size=1,shuffle=True).trainloader
+    for batch,gournd_truth in qa_loader:
+        print(batch)
+        print(gournd_truth)
         break
 
     # simi_loader=eval_dataloader("response_result/ChilleD_StrategyQA_gpt-3.5-turbo-0125_vanilla_QA_2024_05_12.json",1,'compare').loader
