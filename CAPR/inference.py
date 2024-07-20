ae4f4a1cdaccb0cb16cee9ad2cb518997363d3e6
Qmaoboy <hpjhuang@gmail.com>
2024-07-20 16:14:59 +0800

    update

--- CAPR/inference.py
+++ CAPR/inference.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]='1'
+os.environ["CUDA_VISIBLE_DEVICES"]='0'
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from torch import nn
 from accelerate import Accelerator
