1ba335f8e5fa680211029a428e3ab14363bfa9a4
Qmaoboy <hpjhuang@gmail.com>
2024-07-20 16:26:38 +0800

    server5

--- CAPR/RL_env.py
+++ CAPR/RL_env.py
@@ -68,7 +68,7 @@ def reward_function(result_batch,Ground_truth,Document):
         Document_List: Batch* K
     '''
 
-    eval_acc=acc_metric('f1')
+    eval_acc=acc_metric('extract_answer')
     simi=simi_metric("Cos_sim")
     lambda_value=0.7
     ## Balance Between ECE and ACC
